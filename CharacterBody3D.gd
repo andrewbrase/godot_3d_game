c3d788a734df4da17cc91f48dabe9c9641aead74
Andrew Brase <106159556+andrewbrase@users.noreply.github.com>
2024-03-28 23:48:03 -0500

    Update CharacterBody3D.gd

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -5,7 +5,6 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var neck := $Node3D_neck
 @onready var camera := $Node3D_neck/Camera3D
@@ -42,8 +41,7 @@ func _physics_process(delta):
 			print(esc_pressed)
 	
 	if not esc_pressed:	
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
+		
 		var input_dir = Input.get_vector("left", "right", "forward", "back")
 		var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if direction:
